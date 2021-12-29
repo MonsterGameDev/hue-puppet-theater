aa5c975331d9200055bd13c7904d0f5ec63be751
MonsterGameDev <per@brf.dk>
2021-12-29 23:47:29 +0100

    group(room) panel working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,15 +7,17 @@ import { AppState } from '../+state/state.interfaces';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private store: Store<AppState>) { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
     this.store.dispatch(lightsLoadAction());
     this.store.dispatch(groupsLoadAction());
   }
 
+  loadGroups(): void {
+    this.store.dispatch(groupsLoadAction());
+  }
 }
